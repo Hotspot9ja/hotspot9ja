f1fb93928b69559ce9e27246f5bdb83e4a9658aa
sammiearchie77 <chinonsosamuel29@gmail.com>
2022-04-14 00:22:33 +0100

    second commit

---
+++ hotspot/hotspot/urls.py
@@ -0,0 +1,13 @@
+
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    
+    # User management
+    path('accounts/', include('django.contrib.auth.urls')),
+    
+    # local app 
+    path('', include('hotspotapp.urls')),
+]
